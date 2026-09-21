f8d5d78ca8113fab1184b56dfea5864e1c005b58
Junmin Lee <terabyte4096@gmail.com>
2026-09-21 20:02:26 +0900

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { SiteLayout } from "@/components/site-layout";
 export default function Home() {
   return (
     <SiteLayout>
+      <></>
       {/* <Landing /> */}
     </SiteLayout>
   );
